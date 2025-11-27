510ba2a1e1f517473eefeea349efbd42c90e5215
JDelaCruz-10E <delacruz.jam@obmontessori.edu.ph>
2025-11-27 08:59:54 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+from pyscript import display
+
+
+marching_band = {
+    'Club': 'Marching Band',
+    'Description': 'A club where all music enthusiasts meet',
+    'Meeting Time': 'Every Friday 2:30-4:00 PM',
+    'Location': '8th Floor Band Room',
+    'Advisor': 'Mr. Antonio Almine',
+    'Number of Members': 50,
+    'Category': 'Special'
+}
+
+com_arts = {
+    'Club': 'Communication Arts',
+    'Description': 'Communication arts for students who can learn and express themselves through writing, acting, and many more',
+    'Meeting Time': 'Every Monday 2:00-5:00 PM',
+    'Location': 'Room 130',
+    'Advisor': 'Mr. Loreto',
+    'Number of Members': 41,
+    'Category': 'Academic'
+}
+
+socs_sci = {
+    'Club': 'Social Sciences',
+    'Description': 'A club where you learn social sciences',
+    'Meeting Time': 'Every Friday 2:15-4:30 PM',
+    'Location': 'Room 130',
+    'Advisor': 'Ms. Neena Libramonte',
+    'Number of Members': 19,
+    'Category': 'Academic'
+}
+
+
+display()
